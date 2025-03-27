46c0d388e00c932f746bc194867faabee9f09722
Hernan Montero <hernanm842@gmail.com>
2025-03-27 20:39:18 -0300

    avances del challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Movie.destroy_al
+url = "https://tmdb.lewagon.com/movie/top_rated"
+html_doc = URI.open(url).read
+movies = JSON.parse(html_doc)["results"]
+movies.each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie ['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+puts "Seed completado. #{Movie.count} películas creadas."
