f10e2463dc2c68d0057e25fd98778e64bcd28cd4
Hernan Montero <hernanm842@gmail.com>
2025-03-27 13:11:35 -0300

    primeros avances

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@popperjs/core"
+import "bootstrap"
+// app/assets/config/manifest.js
+//= link popper.js
+//= link bootstrap.min.js
